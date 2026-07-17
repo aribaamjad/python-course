3db82e768b567ba114290035efba44d099c34b8a
Ariba Amjad <aribaamjad@gmail.com>
2026-07-17 02:28:19 -0700

    Fix Railway deployment

--- app.py
+++ app.py
@@ -1,37 +1,21 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
-
-# ==========================
-# Home Page
-# ==========================
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
-# ==========================
-# About Page
-# ==========================
-
 @app.route("/about")
 def about():
     return render_template("about.html")
-
-
-# ==========================
-# Contact Page
-# ==========================
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
 
-# ==========================
-# Run Application
-# ==========================
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
