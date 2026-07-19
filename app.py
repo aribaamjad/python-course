f0e63900c229c43fc610a236ef1e19c765b30017
Ariba Amjad <aribaamjad@gmail.com>
2026-07-18 23:51:16 -0700

    Connect HTML pages

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Railway"
+    return render_template("index.html")
 
-@app.route("/test")
-def test():
-    return "Website is working!"
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
