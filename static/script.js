9f6a7c0a0740e2400c717addaf2e68b8b11ea5b5
Ariba Amjad <aribaamjad@gmail.com>
2026-07-24 02:16:50 -0700

    Added category filter

--- static/script.js
+++ static/script.js
@@ -55,3 +55,24 @@ btn.innerHTML="🌙";
 }
 
 }
+function filterTools(category) {
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+
+        if (category === "all") {
+            card.style.display = "block";
+        }
+
+        else if (card.classList.contains(category)) {
+            card.style.display = "block";
+        }
+
+        else {
+            card.style.display = "none";
+        }
+
+    });
+
+}
