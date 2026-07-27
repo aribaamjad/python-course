9b94ce9e0ede02a70f1659fe60035a1a5a287489
Ariba Amjad <aribaamjad@gmail.com>
2026-07-27 03:50:48 -0700

    Added blog page

--- app.py
+++ app.py
@@ -33,6 +33,9 @@ def terms():
 @app.route("/disclaimer")
 def disclaimer():
     return render_template("disclaimer.html")
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=False)
