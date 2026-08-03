759bba1eadfdad4e2a2dd08c808476e8f3ba8f1d
Ariba Amjad <aribaamjad@gmail.com>
2026-08-03 09:02:21 -0700

    Added AI coding assistants article

--- app.py
+++ app.py
@@ -72,6 +72,9 @@ def best_ai_tools_for_youtubers_2026():
 @app.route("/blog/best-ai-tools-for-content-creators-2026")
 def best_ai_tools_for_content_creators_2026():
     return render_template("best-ai-tools-for-content-creators-2026.html")
+@app.route("/blog/best-ai-coding-assistants-2026")
+def best_ai_coding_assistants_2026():
+    return render_template("best-ai-coding-assistants-2026.html")
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=False)
